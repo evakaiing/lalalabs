20108f05d612d053b2bcf2e1e31d38600fe29ec1
evaki <zaitovaeva2@mail.ru>
2024-10-23 17:20:34 +0300

    accepted

--- lab2/3/solution.c
+++ lab2/3/solution.c
@@ -1,121 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdarg.h>
-
-#define BUFFER_SIZE 1024 
+#include <string.h>
 
 typedef enum return_code {
     OK,
     INVALID_INPUT,
     MEMORY_ALLOCATION_ERROR,
-    OVERFLOW_ERROR, 
     FILE_OPENNING_ERROR
 } return_code;
 
-return_code str_str(char* str, char* sub_str, int** lines, int** entrys, int* count_of_entrys, int start_line) {
-    char* p_str = str;
-    char* p_sub_str;
 
-    *count_of_entrys = 0;
 
-    int capacity = 2;
-    int* temp_entryes;
-    int* temp_lines;
-    int current_line = start_line; 
+return_code proccess_file(char* filename, char* substr) {
+    int match_len = 0;  
+    int char_pos = 0;  
+    int line_number = 1;  
+    int start_pos = 1; 
+    int ch;
+    int substr_len = strlen(substr);
+    int there_is_line_break;
+    int cnt_of_line_break = 0;
+    FILE* file = fopen(filename, "r");
 
-    *entrys = (int*)malloc(sizeof(int) * capacity);
-    *lines = (int*)malloc(sizeof(int) * capacity);
-    if (*entrys == NULL || *lines == NULL) {
-        return MEMORY_ALLOCATION_ERROR;
-    }
-
-    while (*p_str != '\0') {
-        if (*p_str == '\n') {
-            current_line++;
-        }
-
-        char* p_temp = p_str;
-        p_sub_str = sub_str;
-
-        while (*p_temp == *p_sub_str && *p_sub_str != '\0') {
-            p_temp++;
-            p_sub_str++;
-        }
-
-        if (*p_sub_str == '\0') { 
-            if (*count_of_entrys == capacity) {
-                capacity *= 2;
-                temp_entryes = (int*)realloc(*entrys, capacity * sizeof(int));
-                temp_lines = (int*)realloc(*lines, capacity * sizeof(int));
-                if (!temp_entryes || !temp_lines) {
-                    free(*entrys);
-                    free(*lines);
-                    return MEMORY_ALLOCATION_ERROR;
-                }
-                *entrys = temp_entryes;
-                *lines = temp_lines;
-            }
-
-            (*entrys)[*count_of_entrys] = p_str - str;
-            (*lines)[*count_of_entrys] = current_line;
-            (*count_of_entrys)++;
-        }
-        ++p_str;
-    }
-    return OK;
-}
-
-return_code proccess_file(const char* filename, char* substr) {
-    FILE* file;
-    char buffer[BUFFER_SIZE + 1];
-    int line_number = 1;
-    int* entrys = NULL;
-    int* lines = NULL;
-    int count_of_entrys = 0;
-    int remaining_bytes = 0;  
-    char remaining_symbols[100] = {0};
-
-    if ((file = fopen(filename, "r")) == NULL) {
+    if (file == NULL) {
         return FILE_OPENNING_ERROR;
     }
 
-    while (!feof(file)) {
-        int read_size = fread(buffer + remaining_bytes, 1, BUFFER_SIZE - remaining_bytes, file);
-        buffer[read_size + remaining_bytes] = '\0';  
+    char* buffer = (char*)malloc(substr_len);  
+    if (buffer == NULL) {
+        fclose(file);
+        return MEMORY_ALLOCATION_ERROR;
+    }
 
-        if (remaining_bytes > 0) {
-            memcpy(buffer, remaining_symbols, remaining_bytes);
-        }
 
-        return_code status_of_finding = str_str(buffer, substr, &lines, &entrys, &count_of_entrys, line_number);
-        if (status_of_finding == MEMORY_ALLOCATION_ERROR) {
-            printf("Memory allocation error\n");
-            fclose(file);
-            return MEMORY_ALLOCATION_ERROR;
-        }
-
-        for (int i = 0; i < count_of_entrys; i++) {
-            printf("Substr found on line %d, position %d in file %s\n", lines[i], entrys[i] + 1, filename);
-        }
-
-        for (int i = 0; i < read_size + remaining_bytes; i++) {
-            if (buffer[i] == '\n') {
-                line_number++;
-            }
-        }
-        // for example substr - ab\na  we're copy the last 3 bytes 
-        if (read_size >= strlen(substr)) {
-            remaining_bytes = strlen(substr) - 1;
-            memcpy(remaining_symbols, buffer + read_size + remaining_bytes - strlen(substr), remaining_bytes);
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            line_number++;
+            char_pos = 0;
         } else {
-            remaining_bytes = read_size;  
-            memcpy(remaining_symbols, buffer, remaining_bytes);
+            char_pos++;  
+        }
+
+
+        if (ch == substr[match_len]) {
+
+            if (ch == '\n') {
+                there_is_line_break = 1;
+                ++cnt_of_line_break;
+            }
+
+            buffer[match_len++] = ch;
+            if (match_len == substr_len) {
+                if (there_is_line_break) {
+                    printf("Substr found on line %d, position %d in file %s\n", line_number - cnt_of_line_break, start_pos, filename);
+                } else {
+                    printf("Substr found on line %d, position %d in file %s\n", line_number, start_pos, filename);
+                }
+                match_len = 0; 
+                start_pos = char_pos + 1;
+                there_is_line_break = 0;
+            }
+        } else {
+            if (match_len > 0) {
+                fseek(file, -match_len, SEEK_CUR);
+                char_pos -= match_len;
+            }
+            match_len = 0;
+            start_pos = char_pos + 1;
         }
     }
 
-    free(entrys);
-    free(lines);
+    free(buffer);
     fclose(file);
 
     return OK;
@@ -144,7 +100,7 @@ return_code find_substr_in_files(char* substr, ...) {
 }
 
 int main() {
-    char* substr = "abaa";
+    char* substr = "ab\r\n\r\na";
     return_code status = find_substr_in_files(substr, "file1.txt", "file2.txt", NULL);
     if (status == FILE_OPENNING_ERROR) {
         printf("Error file opening\n");
--- lab2/8/solution.c
+++ lab2/8/solution.c
@@ -10,7 +10,8 @@ typedef enum return_code {
     MEMORY_ALLOCATION_ERROR,
     OVERFLOW_ERROR, 
     FILE_OPENNING_ERROR,
-    TRANSFER_NEGATIVE_NUMBER_ATTEMPT
+    TRANSFER_NEGATIVE_NUMBER_ATTEMPT,
+    DEREFERENCE_NULL_POINTER_ATTEMPT
 } return_code;
 
 return_code convert_str_to_num_in_current_base(char* str, int* result, const int base) {
@@ -29,16 +30,27 @@ return_code convert_str_to_num_in_current_base(char* str, int* result, const int
     return OK;
 }
 
-// char* str_reverse(char* str) {
-//     char* p_str = str;
-//     char* p_end_str = str + strlen(str);
+return_code char_to_digit(char ch, int base) {
+    if (ch >= '0' && ch <= '9') {
+        return ch - '0'; 
+    } else if (ch >= 'A' && ch <= 'Z') {
+        return ch - 'A' + 10; 
+    } else if (ch >= 'a' && ch <= 'z') {
+        return ch - 'a' + 10; 
+    } else {
+        return INVALID_INPUT; 
+    }
+}
 
-//     while(p_str != '\0') {
-//         *p_str++ = *p_end_str--;
-//     }
-    
-//     return str;
-// }
+char digit_to_char(int digit) {
+    if (digit >= 0 && digit <= 9) {
+        return digit + '0';  
+    } else if (digit >= 10 && digit < 36) {
+        return digit - 10 + 'a';  
+    } else {
+        return ' ';  
+    }
+}
 
 
 return_code column_addition(char** result, char* first_num, char* second_num, const int base) {
@@ -53,29 +65,30 @@ return_code column_addition(char** result, char* first_num, char* second_num, co
         return MEMORY_ALLOCATION_ERROR;
     }
 
+
     p_first_num = first_num + len1 - 1;
     p_second_num = second_num + len2 - 1;
-    p_result = *result + max_len;
-
-    *(p_result + 1) = '\0';  
+    p_result = *result + max_len;  
+    *(p_result + 1) = '\0'; 
 
     while (p_first_num >= first_num || p_second_num >= second_num || carry != 0) {
-
-        int digit1 = (p_first_num >= first_num) ? *p_first_num-- - '0' : 0;
-        int digit2 = (p_second_num >= second_num) ? *p_second_num-- - '0' : 0;
+        int digit1 = (p_first_num >= first_num) ? char_to_digit(*p_first_num--, base) : 0;
+        int digit2 = (p_second_num >= second_num) ? char_to_digit(*p_second_num--, base) : 0;
 
         if (digit1 >= base || digit2 >= base) {
+            free(*result);
             return INVALID_INPUT;
         }
 
         int sum = digit1 + digit2 + carry;
+        carry = sum / base;  
 
-        carry = sum / base;
-
-        *p_result-- = (sum % base) + '0';  
+        *p_result-- = digit_to_char(sum % base);
     }
+
+
     if (carry != 0) {
-        *p_result = carry + '0';
+        *p_result = digit_to_char(carry);
     } else {
         p_result++;
     }
@@ -85,58 +98,84 @@ return_code column_addition(char** result, char* first_num, char* second_num, co
     }
 
     memmove(*result, p_result, strlen(p_result) + 1);
-    
+
     return OK;
 }
 
 
-return_code summation(int* result, int base, int count, ...) {
+return_code summation(char** result, int base, int count, ...) {
     va_list args;
     int i;
-    char final_str[BUFSIZ] = "0";
-    char* result_cur;
+    char* final_str = NULL;
+    char* result_cur = NULL;
+    int capacity = 2;
 
     if (base < 2 || base > 36) {
         return INVALID_INPUT;
     }
 
+    final_str = (char*)malloc(capacity);
+    if (!final_str) {
+        return MEMORY_ALLOCATION_ERROR;
+    }
+    strcpy(final_str, "0");
+
     va_start(args, count);
 
     for (i = 0; i < count; ++i) {
-        char* str;
-        str = va_arg(args, char*);
-        
+        char* str = va_arg(args, char*);
+
         if (str[0] == '-') {
+            free(final_str);
+            va_end(args);
             return TRANSFER_NEGATIVE_NUMBER_ATTEMPT;
         }
 
-
         return_code st = column_addition(&result_cur, str, final_str, base);
         if (st != OK) {
+            free(final_str);
             va_end(args);
             return st;
         }
 
+        int new_length = strlen(result_cur) + 1;
+        if (new_length > capacity) {
+            capacity = new_length * 2;
+            char* temp = realloc(final_str, capacity);
+            if (!temp) {
+                free(result_cur);
+                free(final_str);
+                va_end(args);
+                return MEMORY_ALLOCATION_ERROR;
+            }
+            final_str = temp;
+        }
+
         strcpy(final_str, result_cur);
-        free(result_cur);
+        free(result_cur);  
     }
 
     va_end(args);
 
-    return_code status_of_convertion = convert_str_to_num_in_current_base(final_str, result, base);
-    if (status_of_convertion != OK) {
-        return status_of_convertion;
+    *result = (char*)malloc(strlen(final_str) + 1);
+    if (!(*result)) {
+        free(final_str);
+        return MEMORY_ALLOCATION_ERROR;
     }
+    strcpy(*result, final_str);
+
+    free(final_str);  
 
     return OK;
 }
 
 
-int main() {
-    int result1 = 0;
-    int result2 = 0;
 
-    return_code status = summation(&result1, 2, 2, "101", "111100", "10011");
+int main() {
+    char* result1;
+    char* result2;
+
+    return_code status = summation(&result1, 2, 2, "101", "111100");
 
     if (status == INVALID_INPUT) {
         printf("Invalid input of number\n");
@@ -147,9 +186,9 @@ int main() {
     } else if (status == TRANSFER_NEGATIVE_NUMBER_ATTEMPT) {
         printf("Requires not a negative number\n");
     }
-    printf("%d\n", result1);
+    printf("%s\n", result1);
 
-    return_code status2 = summation(&result2, 10, 2, "00123", "");
+    return_code status2 = summation(&result2, 36, 2, "zzzzzzzzzzzzzzzzzzzzzzzz", "zzzzzzzzzzzzzzzzzzzz");
 
     if (status2 == INVALID_INPUT) {
         printf("Invalid input of number\n");
@@ -160,5 +199,5 @@ int main() {
     } else if (status2 == TRANSFER_NEGATIVE_NUMBER_ATTEMPT) {
         printf("Requires not a negative number\n");
     }
-    printf("%d\n", result2);
+    printf("%s\n", result2);
 }
